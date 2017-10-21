1b2e4be0e8b53aa42a991d7461d3788cefab8907
Ashish Sinha <ashishsinha108@gmail.com>
2017-10-21 16:22:26 +0530

    regression done

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -29,7 +29,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,11 +38,12 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
-
-
-
-
-
+from sklearn.linear_model import LinearRegression as LR
+reg=LR().fit(feature_train,target_train)
+print "Coeff: ",reg.coef_
+print "intercept: ",reg.intercept_
+print "Train Score: " ,reg.score(feature_train,target_train)
+print "Test Score: ",reg.score(feature_test,target_test)
 
 
 ### draw the scatterplot, with color-coded training and testing points
@@ -61,9 +62,12 @@ plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
 
 ### draw the regression line, once it's coded
 try:
-    plt.plot( feature_test, reg.predict(feature_test) )
+    plt.plot( feature_test, reg.predict(feature_test),color='black' )
 except NameError:
     pass
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b") 
+print reg.coef_
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
